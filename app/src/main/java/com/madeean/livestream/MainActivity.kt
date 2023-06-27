82482631361de440a84b90472328c14df66ae16e
MadeReihan <reihanmade@gmail.com>
2023-06-27 16:14:23 +0700

    implement youtube view library

--- app/src/main/java/com/madeean/livestream/MainActivity.kt
+++ app/src/main/java/com/madeean/livestream/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.madeean.livestream
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.madeean.livestream.databinding.ActivityMainBinding
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions
+
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var binding:ActivityMainBinding
   override fun onCreate(savedInstanceState: Bundle?) {
+    binding = ActivityMainBinding.inflate(layoutInflater)
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(binding.root)
+
+    lifecycle.addObserver(binding.youtubePlayerView)
+
+    val customPlayerUi: View =binding.youtubePlayerView.inflateCustomPlayerUi(R.layout.custom_player_ui)
+
+    val listener: YouTubePlayerListener = object : AbstractYouTubePlayerListener() {
+      override fun onReady(youTubePlayer: YouTubePlayer) {
+        val customPlayerUiController:CustomPlayerUiController = CustomPlayerUiController(
+          this@MainActivity,
+          customPlayerUi,
+          youTubePlayer,
+          binding.youtubePlayerView
+        )
+        customPlayerUiController.init()
+        youTubePlayer.addListener(customPlayerUiController)
+        youTubePlayer.loadVideo("yabDCV4ccQs",0f)
+      }
+
+    }
+
+    val options:IFramePlayerOptions = IFramePlayerOptions.Builder().controls(0).build()
+    binding.youtubePlayerView.initialize(listener,options)
+
   }
 }
