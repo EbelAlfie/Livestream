c81ce7ea1a2ed048e51f31ee2c8039876377e158
EbelAlfie <davis.onde.wei@gmail.com>
2023-07-04 16:51:19 +0700

    added interface

--- app/src/main/java/com/madeean/livestream/data/repository/LivestreamRepositoryImpl.kt
+++ app/src/main/java/com/madeean/livestream/data/repository/LivestreamRepositoryImpl.kt
@@ -1,14 +1,24 @@
 package com.madeean.livestream.data.repository
 
 import com.madeean.livestream.data.model.LivestreamDataModel
+import com.madeean.livestream.data.model.LivestreamViewCountModel
 import com.madeean.livestream.data.remotesource.RetrofitObj
-import com.madeean.livestream.domain.entity.LivestreamData
-import kotlinx.coroutines.flow.Flow
+import com.madeean.livestream.domain.entity.LivestreamKeysData
+import com.madeean.livestream.domain.entity.LivestreamStatistic
 
 class LivestreamRepositoryImpl: LivestreamRepository {
-    override suspend fun getLivestreamData(): List<LivestreamData> {
+    override suspend fun getLivestreamData(): List<LivestreamKeysData> {
         val data = RetrofitObj.apiService().getLiveStreamData()
-        return LivestreamDataModel.transform(data ?: listOf())
+        return LivestreamDataModel.transform(data)
+    }
+
+    override suspend fun getLivestreamViewCount(): Int {
+        val data = RetrofitObj.apiService().getLiveStreamView()
+        return LivestreamViewCountModel.transform(data)
+    }
+
+    override suspend fun postViewCount(liveStat: LivestreamStatistic) {
+        val data = RetrofitObj.apiService().postViewCount()
     }
 
 }
