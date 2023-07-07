140b772b8356a63ef2d3bfa9280b6ece957f2be6
DavisInde <davis.onde.wei@gmail.com>
2023-07-07 10:23:13 +0700

    cherry pick

--- app/src/main/java/com/madeean/livestream/view/FragmentLiveStreaming.kt
+++ app/src/main/java/com/madeean/livestream/view/FragmentLiveStreaming.kt
@@ -4,40 +4,34 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
 import androidx.media3.common.PlaybackException
 import androidx.media3.common.Player
-import androidx.media3.common.Timeline.Window
-import androidx.media3.datasource.DataSource
-import androidx.media3.datasource.DefaultHttpDataSource
 import androidx.media3.datasource.rtmp.RtmpDataSource
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.dash.DashMediaSource
-import androidx.media3.exoplayer.offline.DownloadHelper.createMediaSource
 import androidx.media3.exoplayer.source.MediaSource
 import androidx.media3.exoplayer.source.ProgressiveMediaSource
 import com.madeean.livestream.databinding.CustomPlayerUiBinding
-import com.madeean.livestream.domain.entity.LivestreamKeysData
-import com.madeean.livestream.domain.products.model.ModelProductList
+import com.madeean.livestream.domain.products.model.ModelProductListDomain
 import com.madeean.livestream.viewmodel.FragmentLiveViewModel
-import com.madeean.livestream.viewmodel.LivestreamViewModel
+import com.madeean.livestream.viewmodel.ProductsViewModel
 
 class FragmentLiveStreaming(private val port: Int, private val streamKey: String) : Fragment() {
   private lateinit var binding: CustomPlayerUiBinding
   private val viewCountHandler = Handler(Looper.getMainLooper())
   private lateinit var viewModel: FragmentLiveViewModel
-    private val BASE_URL: String = "rtmp://0.tcp.ap.ngrok.io:$port/live/" //"https://livesim.dashif.org/livesim/chunkdur_1/ato_7/testpic4_8s/Manifest.mpd"//"rtmp://0.tcp.ap.ngrok.io:$port/live/"
-    private lateinit var listData:ArrayList<ModelProductList>
+  private lateinit var productViewModel: ProductsViewModel
+  private val BASE_URL: String =
+    "rtmp://0.tcp.ap.ngrok.io:$port/live/" //"https://livesim.dashif.org/livesim/chunkdur_1/ato_7/testpic4_8s/Manifest.mpd"//"rtmp://0.tcp.ap.ngrok.io:$port/live/"
+  private var listData: ArrayList<ModelProductListDomain> = arrayListOf()
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
@@ -103,41 +97,44 @@ class FragmentLiveStreaming(private val port: Int, private val streamKey: String
   private fun initPlayer() {
     val exoplayer = ExoPlayer.Builder(requireContext()).build()
     exoplayer.apply {
-            binding.pvVideoView.player = this
+      binding.pvVideoView.player = this
 
-            toastPrint(BASE_URL + streamKey)
+      toastPrint(BASE_URL + streamKey)
 
-            val mediaItem = buildMediaItem(BASE_URL + streamKey)
+      val mediaItem = buildMediaItem(BASE_URL + streamKey)
 
-            /**RTMP*/
-            val datasource = RtmpDataSource.Factory() //transfer listener
-            val mediaSource: MediaSource = ProgressiveMediaSource.Factory(datasource)
-                .createMediaSource(mediaItem)
-            /**DASH*/
-            /*val dataSourceFactory: DataSource.Factory = DefaultHttpDataSource.Factory()
-            // Create a dash media source pointing to a dash manifest uri.
-            val mediaSource: MediaSource =
-                DashMediaSource.Factory(dataSourceFactory).createMediaSource(mediaItem)
-            */
+      /**RTMP*/
+      val datasource = RtmpDataSource.Factory() //transfer listener
+      val mediaSource: MediaSource = ProgressiveMediaSource.Factory(datasource)
+        .createMediaSource(mediaItem)
+      /**DASH*/
+      /*val dataSourceFactory: DataSource.Factory = DefaultHttpDataSource.Factory()
+      // Create a dash media source pointing to a dash manifest uri.
+      val mediaSource: MediaSource =
+          DashMediaSource.Factory(dataSourceFactory).createMediaSource(mediaItem)
+      */
       addListener(object : Player.Listener {
         override fun onIsLoadingChanged(isLoading: Boolean) {
           super.onIsLoadingChanged(isLoading)
-                    showProgressBar(binding.progressLoading, isLoading)
+          showProgressBar(binding.progressLoading, isLoading)
         }
 
         override fun onPlaybackStateChanged(playbackState: Int) {
           when (playbackState) {
             Player.STATE_READY -> {
-                            showProgressBar(binding.progressLoading, false)
-                            if (exoplayer.isCommandAvailable(
-                                Player.COMMAND_SEEK_TO_DEFAULT_POSITION)) {
+              showProgressBar(binding.progressLoading, false)
+              if (exoplayer.isCommandAvailable(
+                  Player.COMMAND_SEEK_TO_DEFAULT_POSITION
+                )
+              ) {
                 seekToDefaultPosition()
-                                }
+              }
               toastPrint("ready")
             }
             Player.STATE_BUFFERING -> {
-                            //seekTo(currentLiveOffset)
-                            toastPrint("buffer")}
+              //seekTo(currentLiveOffset)
+              toastPrint("buffer")
+            }
             Player.STATE_ENDED -> {
               toastPrint("end")
               showEndStream(binding)
@@ -152,31 +149,31 @@ class FragmentLiveStreaming(private val port: Int, private val streamKey: String
         override fun onPlayerError(error: PlaybackException) {
           super.onPlayerError(error)
           toastPrint(error.toString())
-                    prepare()
+          prepare()
         }
       })
 
-            // Update the track selection parameters to only pick standard definition tracks
-            trackSelectionParameters = trackSelectionParameters
-                .buildUpon()
-                .setMaxVideoSizeSd()
-                .build()
+      // Update the track selection parameters to only pick standard definition tracks
+      trackSelectionParameters = trackSelectionParameters
+        .buildUpon()
+        .setMaxVideoSizeSd()
+        .build()
 
-            setMediaSource(mediaSource)
+      setMediaSource(mediaSource)
       prepare()
       playWhenReady = true
 
     }
   }
 
-    private fun Long.toSecond(): Long {
-        return this/ 1000000
-    }
+  private fun Long.toSecond(): Long {
+    return this / 1000000
+  }
 
   private fun buildMediaItem(url: String): MediaItem {
-        return MediaItem.Builder()
-            .setUri(url)
-            .setMimeType(MimeTypes.APPLICATION_MATROSKA)
+    return MediaItem.Builder()
+      .setUri(url)
+      .setMimeType(MimeTypes.APPLICATION_MATROSKA)
       .setLiveConfiguration(
         MediaItem.LiveConfiguration.Builder().setMaxPlaybackSpeed(1.02f).build()
       )
@@ -199,9 +196,9 @@ class FragmentLiveStreaming(private val port: Int, private val streamKey: String
     binding.tvStreamEnd.visibility = View.VISIBLE
   }
 
-    private fun showProgressBar(progressBar: ProgressBar, isLoading: Boolean) {
-        progressBar.visibility = if (isLoading) View.VISIBLE else View.INVISIBLE
-    }
+  private fun showProgressBar(progressBar: ProgressBar, isLoading: Boolean) {
+    progressBar.visibility = if (isLoading) View.VISIBLE else View.INVISIBLE
+  }
 
   private fun toastPrint(s: String) {
     Toast.makeText(requireContext(), s, Toast.LENGTH_SHORT).show()
